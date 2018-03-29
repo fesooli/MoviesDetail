3f31823f1b93f931a805020a84ff75045ddd499b
Usuário Convidado <Guest@PA06MICRO06.local>
2018-03-28 22:56:52 -0300

    initial commit

---
+++ MoviesLib/MoviesLib/Movie.swift
@@ -0,0 +1,40 @@
+//
+//  Movie.swift
+//  MoviesLib
+//
+//  Created by Usuário Convidado on 28/03/18.
+//  Copyright © 2018 EricBrito. All rights reserved.
+//
+
+import Foundation
+
+struct Movie: Codable {
+    
+    let title: String
+    let categories: [String]
+    let duration: String
+    let rating: Double
+    let summary: String
+    let imageName: String
+    
+    var imageSmall: String {
+        return imageName + "-small.jpg"
+    }
+    
+    var imageWide: String {
+        return imageName + "-wide.jpg"
+    }
+    
+    var categoriesDescription: String {
+        return categories.joined(separator: " | ")
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case categories
+        case duration
+        case rating
+        case summary
+        case imageName = "image_name"
+    }
+}
---
+++ MoviesLib/MoviesLib/MovieViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  MoviesLib
+//
+//  Created by Eric on 06/03/17.
+//  Copyright © 2017 EricBrito. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    // MARK: IBOutlets
+    @IBOutlet weak var ivPoster: UIImageView!
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbGenre: UILabel!
+    @IBOutlet weak var lbDuration: UILabel!
+    @IBOutlet weak var lbScore: UILabel!
+    @IBOutlet weak var tvSinopsis: UITextView!
+    @IBOutlet weak var lcButtonX: NSLayoutConstraint!
+    
+    var movie: Movie!
+    
+    // MARK: Super Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ivPoster.image = UIImage(named: movie.imageWide)
+        lbTitle.text = movie.title
+        tvSinopsis.text = movie.summary
+        lbScore.text = "⭐️ \(movie.rating)/10"
+        lbGenre.text = movie.categoriesDescription
+    }
+    
+}
